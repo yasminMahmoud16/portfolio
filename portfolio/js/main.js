a9cfebcc24d70d388d1c1a2813c84cadd611793a
yasminMahmoud16 <153022052+yasminMahmoud16@users.noreply.github.com>
2024-10-22 15:16:01 +0300

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,10 @@
+
+// typed animation
+
+var typed = new Typed('#typed-txt', {
+    strings: ['Designer', 'Developer','Freelancer','Photographer'],
+
+    typeSpeed: 80,
+    loop: true,
+    html: true
+});
